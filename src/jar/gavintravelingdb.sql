3816517e01e11d754503547c009970954bc19507
Max <Maxfalk_95@hotmail.com>
2020-04-24 16:42:36 +0200

    Changes in  database
    Added data

--- src/jar/gavintravelingdb.sql
+++ src/jar/gavintravelingdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 24 apr 2020 kl 13:48
+-- Tid vid skapande: 24 apr 2020 kl 16:41
 -- Serverversion: 10.4.11-MariaDB
 -- PHP-version: 7.4.3
 
@@ -45,8 +45,22 @@ INSERT INTO `booked_rooms` (`booking_id`, `room_id`, `extra_bed`, `start_date`, 
 (2, 2, 2, '2020-06-24 15:00:00', '2020-06-29 11:00:00'),
 (3, 3, 0, '2020-06-01 15:00:00', '2020-06-25 12:00:00'),
 (4, 3, 2, '2020-06-12 15:00:00', '2020-07-05 11:00:00'),
-(5, 2, 1, '2020-07-01 15:00:00', '2020-07-04 11:00:00'),
-(6, 6, 1, '2020-06-19 00:00:00', '2020-06-25 00:00:00');
+(5, 2, 1, '2020-06-01 15:00:00', '2020-06-18 11:00:00'),
+(6, 6, 1, '2020-06-19 00:00:00', '2020-07-25 00:00:00'),
+(7, 7, 1, '2020-07-18 00:00:00', '2020-08-25 00:00:00'),
+(8, 8, 1, '2020-06-17 00:00:00', '2020-06-24 00:00:00'),
+(9, 9, 1, '2020-07-21 00:00:00', '2020-07-25 00:00:00'),
+(10, 10, 1, '2020-06-23 00:00:00', '2020-07-01 00:00:00'),
+(11, 11, 1, '2020-06-19 00:00:00', '2020-06-25 00:00:00'),
+(12, 12, 1, '2020-07-24 00:00:00', '2020-08-28 00:00:00'),
+(13, 13, 1, '2020-06-11 00:00:00', '2020-08-22 00:00:00'),
+(14, 14, 1, '2020-08-11 00:00:00', '2020-09-14 00:00:00'),
+(15, 15, 1, '2020-08-24 00:00:00', '2020-09-28 00:00:00'),
+(16, 16, 1, '2020-06-22 00:00:00', '2020-06-24 00:00:00'),
+(17, 17, 1, '2020-09-19 00:00:00', '2020-10-25 00:00:00'),
+(18, 18, 1, '2020-08-19 00:00:00', '2020-09-25 00:00:00'),
+(19, 19, 1, '2020-04-12 00:00:00', '2020-09-21 00:00:00'),
+(20, 20, 1, '2020-08-04 00:00:00', '2020-09-11 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -60,23 +74,34 @@ CREATE TABLE `bookings` (
   `person_count` int(11) UNSIGNED NOT NULL,
   `children_count` int(11) UNSIGNED NOT NULL,
   `small_children_count` int(11) UNSIGNED NOT NULL,
-  `active_booking` bit(1) NOT NULL,
-  `full_pension` int(3) UNSIGNED NOT NULL,
-  `Half_pension` int(3) UNSIGNED NOT NULL,
-  `all_inclusive` int(3) UNSIGNED NOT NULL
+  `active_booking` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumpning av Data i tabell `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `customer_id`, `person_count`, `children_count`, `small_children_count`, `active_booking`, `full_pension`, `Half_pension`, `all_inclusive`) VALUES
-(1, 1, 2, 2, 1, b'0', 0, 0, 0),
-(2, 2, 2, 1, 1, b'1', 0, 0, 0),
-(3, 3, 2, 2, 1, b'1', 0, 0, 0),
-(4, 4, 2, 0, 2, b'1', 0, 0, 0),
-(5, 5, 1, 0, 0, b'1', 0, 0, 0),
-(6, 6, 2, 2, 1, b'1', 0, 0, 0);
+INSERT INTO `bookings` (`id`, `customer_id`, `person_count`, `children_count`, `small_children_count`, `active_booking`) VALUES
+(1, 1, 2, 2, 1, b'0'),
+(2, 2, 2, 1, 1, b'1'),
+(3, 3, 2, 2, 1, b'1'),
+(4, 4, 2, 0, 2, b'1'),
+(5, 5, 1, 0, 0, b'1'),
+(6, 6, 1, 2, 1, b'0'),
+(7, 7, 3, 1, 0, b'0'),
+(8, 8, 2, 1, 0, b'0'),
+(9, 9, 1, 2, 2, b'0'),
+(10, 10, 1, 2, 2, b'0'),
+(11, 11, 1, 1, 2, b'0'),
+(12, 12, 2, 2, 0, b'0'),
+(13, 13, 3, 3, 0, b'0'),
+(14, 14, 3, 1, 2, b'0'),
+(15, 15, 2, 2, 2, b'0'),
+(16, 16, 1, 2, 0, b'0'),
+(17, 17, 3, 1, 2, b'0'),
+(18, 18, 2, 4, 0, b'0'),
+(19, 19, 1, 2, 1, b'0'),
+(20, 20, 1, 1, 1, b'0');
 
 -- --------------------------------------------------------
 
@@ -99,8 +124,21 @@ INSERT INTO `cities` (`id`, `name`) VALUES
 (3, 'dallas'),
 (4, 'sheregesh'),
 (5, 'banyumas'),
-(6, 'Royceshire'),
-(7, 'Lake Toby');
+(6, 'West Felicityton'),
+(7, 'Corkerybury'),
+(8, 'North Nannieport'),
+(9, 'East Hassieborough'),
+(10, 'South Deon'),
+(11, 'Ronnychester'),
+(12, 'New Otiliaview'),
+(13, 'Port Damian'),
+(14, 'Gutmannshire'),
+(15, 'North Jaunitaside'),
+(16, 'South Haroldfurt'),
+(17, 'New Jack'),
+(18, 'East Miguelfort'),
+(19, 'Kshlerinburgh'),
+(20, 'O\'Keefechester');
 
 -- --------------------------------------------------------
 
@@ -128,8 +166,21 @@ INSERT INTO `customers` (`id`, `first_name`, `last_name`, `phone_number`, `e_mai
 (3, 'paulita', 'harverson', '9978691640', 'pharverson2@prlog.org', 'ownjzsu', '1990150778'),
 (4, 'garfield', 'tremellier', '1555529808', 'gtremellier3@microsoft.com', '7rx5r7dl', '1974401779'),
 (5, 'test', 'test', '0739990855', 'test@mail.com', '$2a$10$B789zZwhK1dWpie0CkVCvOfPVLBiky7iaQ2/41KnfwPZr.gGHYbaW', '9999999'),
-(6, 'vanya', 'atyea', '6578857057', 'vatyea4@purevolume.com', 'jizozevgzzsk', '1986505981'),
-(7, 'Tyree', 'Bednar', '005-503-8853', 'Waldo52@hotmail.com', 'MHhCeXp6ZGnVuXx', '1989203832');
+(6, 'Reed', 'Bechtelar', '890-102-8137', 'Delaney33@hotmail.com', 'D0R0e3H4q8vWUHw', '1993746294'),
+(7, 'Leola', 'Conroy', '776-519-4548', 'Breana35@gmail.com', 'mOJiwMYyyM7Z2BV', '1992991172'),
+(8, 'Elliot', 'Hyatt', '291-303-4807', 'Bryce_Walker2@hotmail.com', 'tASKPVUxocx8GGq', '1991316087'),
+(9, 'Bartholome', 'O\'Hara', '015-186-4617', 'Maegan_Bergnaum52@gmail.com', 'baw6k7RD1v7eN8i', '1997898815'),
+(10, 'Lawrence', 'Klein', '643-571-9418', 'Justice_Jones@gmail.com', 'rBOwHHLIbxwtvQd', '1998101065'),
+(11, 'Danyka', 'Conroy', '990-908-7293', 'Lyric.Gerlach79@hotmail.com', 'tT4oC6BjLEpad10', '1992732616'),
+(12, 'Polly', 'Wisoky', '069-897-1896', 'Bernhard_Legros93@yahoo.com', 'Xuy9csfKxkLwXzn', '1996651762'),
+(13, 'Nathan', 'Hintz', '185-443-8762', 'Nikita.Auer@hotmail.com', 'Cv8FKYH9R9BknOk', '1990230006'),
+(14, 'Richie', 'Berge', '939-543-2575', 'Xavier.Beer64@gmail.com', 'rI7l_pKAWllVv7d', '1995799533'),
+(15, 'Armani', 'Okuneva', '958-309-3894', 'Shane.Klein@hotmail.com', 'NdvYY5jNAQAoAjx', '1994111129'),
+(16, 'Vidal', 'Breitenberg', '208-205-9042', 'Lucio86@gmail.com', '2GJpJNFWj4H7SVN', '1998538928'),
+(17, 'Albina', 'Johnston', '499-929-3348', 'Arely_Wisozk90@hotmail.com', 'FzWzFFSG5ZkaMYv', '1990906305'),
+(18, 'Terence', 'Hilpert', '582-659-6359', 'Lisette_Jacobi34@gmail.com', '9ogqBLhxJHPI9sd', '1990722602'),
+(19, 'Percival', 'Lemke', '627-229-9042', 'Armand.Harber59@gmail.com', 'W4b2VURpet2PB3G', '1994593304'),
+(20, 'Lowell', 'Kertzmann', '895-834-1459', 'Carmelo12@hotmail.com', '0moEId2XzL75kJd', '1997885198');
 
 -- --------------------------------------------------------
 
@@ -164,12 +215,26 @@ CREATE TABLE `hotels` (
 --
 
 INSERT INTO `hotels` (`id`, `name`, `stars`, `city_id`, `picture`, `phone`, `e_mail`, `address`, `postal_code`, `pool`, `night_entertainment`, `restaurant`, `children_club`, `distance_to_beach`, `distance_to_center`, `pension_half_price`, `pension_full_price`, `all_inclusive`, `extra_bed`) VALUES
-(1, 'roberts-heller', 1, 1, '', '647 946 9787', 'gglendinning0@dagondesign.com', '4th', '26653', b'1', b'1', b'1', b'0', 2, 20, 200, 300, 400, 0),
-(2, 'gorczany inc', 1, 2, '', '458 249 3466', 'fstrowger1@mayoclinic.com', 'ohio', '26787', b'1', b'0', b'0', b'0', 8, 2, 200, 300, 400, 0),
-(3, 'bartoletti and sons', 3, 3, '', '208 116 2957', 'asyers2@people.com.cn', 'john wall', '26467', b'1', b'0', b'0', b'0', 11, 7, 200, 300, 400, 0),
-(4, 'hessel group', 2, 4, '', '613 631 2948', 'xmulvaney3@cargocollective.com', 'hoepker', '26683', b'0', b'1', b'0', b'1', 15, 5, 200, 300, 400, 0),
-(5, 'predovic-cassin', 3, 5, '', '800 863 4709', 'ebownes4@issuu.com', 'farmco', '26685', b'1', b'1', b'0', b'0', 20, 11, 200, 300, 400, 0),
-(6, 'Dach - Lehner', 4, 7, '', '498-955-8002', 'Garrett.Lang90@gmail.com', '13th', '52253', b'1', b'1', b'1', b'0', 4, 20, 200, 300, 400, 0);
+(1, 'roberts-heller', 1, 1, '', '647 946 9787', 'gglendinning0@dagondesign.com', '4th', '26653', b'1', b'1', b'1', b'0', 2, 20, 100, 200, 300, 0),
+(2, 'gorczany inc', 1, 2, '', '458 249 3466', 'fstrowger1@mayoclinic.com', 'ohio', '26787', b'1', b'1', b'0', b'0', 8, 2, 100, 200, 300, 0),
+(3, 'bartoletti and sons', 3, 3, '', '208 116 2957', 'asyers2@people.com.cn', 'john wall', '26467', b'1', b'1', b'0', b'0', 11, 7, 100, 200, 300, 0),
+(4, 'hessel group', 2, 4, '', '613 631 2948', 'xmulvaney3@cargocollective.com', 'hoepker', '26683', b'0', b'1', b'0', b'1', 15, 5, 100, 200, 300, 0),
+(5, 'predovic-cassin', 3, 5, '', '800 863 4709', 'ebownes4@issuu.com', 'farmco', '26685', b'1', b'1', b'0', b'0', 20, 11, 100, 200, 300, 0),
+(6, 'Johnson Inc', 2, 6, '', '722-251-7531', 'Cesar84@gmail.com', '433th', '22253', b'1', b'0', b'0', b'1', 2, 3, 100, 200, 300, 0),
+(7, 'Flatley Group', 2, 7, '', '628-735-4493', 'Alexander.Kautzer54@yahoo.com', '3213th', '42253', b'1', b'1', b'0', b'1', 3, 1, 100, 200, 300, 0),
+(8, 'Russel - Halvorson', 2, 8, '', '737-894-7729', 'Kyleigh_Ondricka30@yahoo.com', '33th', '12253', b'1', b'0', b'1', b'1', 1, 3, 100, 200, 300, 0),
+(9, 'Robel - Marquardt', 2, 10, '', '317-167-6363', 'Ricardo95@hotmail.com', '631th', '25233', b'1', b'1', b'0', b'1', 15, 15, 100, 200, 300, 0),
+(10, 'Robel - Mayert', 2, 10, '', '522-548-2996', 'Hayden_Grady19@yahoo.com', '923th', '25153', b'1', b'0', b'1', b'1', 2, 1, 100, 200, 300, 0),
+(11, 'Brekke Inc', 2, 11, '', '982-395-4403', 'Gillian.Stamm@hotmail.com', '4233th', '12253', b'1', b'0', b'0', b'1', 6, 12, 100, 200, 300, 0),
+(12, 'Hettinger - Torphy', 2, 12, '', '990-711-3531', 'Nathaniel.Walsh@yahoo.com', '123th', '17253', b'1', b'0', b'1', b'1', 3, 16, 100, 200, 300, 0),
+(13, 'Walter and Sons', 2, 14, '', '031-920-4156', 'Letitia_Zulauf14@hotmail.com', '313th', '65253', b'1', b'0', b'1', b'1', 12, 2, 100, 200, 300, 0),
+(14, 'Roberts LLC', 2, 15, '', '648-624-5683', 'Madisyn.Witting10@hotmail.com', '223th', '65453', b'1', b'0', b'0', b'1', 3, 6, 100, 200, 300, 0),
+(15, 'McGlynn Inc', 2, 15, '', '687-293-1942', 'Caroline79@gmail.com', '133th', '45253', b'1', b'1', b'1', b'1', 2, 12, 100, 200, 300, 0),
+(16, 'Wintheiser - Hyatt', 2, 17, '', '048-069-5054', 'Rick.Cole@yahoo.com', '43th', '96253', b'1', b'0', b'0', b'1', 12, 13, 100, 200, 300, 0),
+(17, 'Schaefer and Sons', 2, 19, '', '619-764-5992', 'Tyra.Prosacco9@gmail.com', '563th', '97253', b'1', b'0', b'0', b'1', 2, 1, 100, 200, 300, 0),
+(18, 'Howell Inc', 2, 20, '', '285-669-0486', 'Mylene_Hintz@yahoo.com', '893th', '33253', b'1', b'1', b'1', b'1', 7, 7, 100, 200, 300, 0),
+(19, 'Bins - Schoen', 2, 20, '', '634-277-7528', 'Berry59@yahoo.com', '889th', '46753', b'1', b'1', b'0', b'1', 7, 17, 100, 200, 300, 0),
+(20, 'Koelpin - Hamill', 2, 20, '', '299-287-6227', 'Reginald26@hotmail.com', '663th', '13053', b'1', b'1', b'1', b'1', 16, 12, 100, 200, 300, 0);
 
 -- --------------------------------------------------------
 
@@ -194,7 +259,21 @@ INSERT INTO `reviews` (`id`, `rating`, `comment`, `booking_id`) VALUES
 (3, 3, 'Jag gillar ost', 3),
 (4, 4, 'bra arbetat!', 4),
 (5, 2, 'dålig service', 5),
-(6, 2, 'dfggfsdsdfgfsgdfgsd', 6);
+(6, 3, 'quo voluptas nulla pariatur', 6),
+(7, 4, 'nulla pariatur', 7),
+(8, 1, 'quo voluptas', 8),
+(9, 2, 'voluptas nulla', 9),
+(10, 4, 'nulla quo', 10),
+(11, 3, 'voluptas nice', 11),
+(12, 5, 'nicevoluptas', 12),
+(13, 1, 'nivoluptasce', 13),
+(14, 2, 'npariaturice', 14),
+(15, 4, 'nipariaturce', 15),
+(16, 3, 'nipariatuturce', 16),
+(17, 1, 'ariatu', 17),
+(18, 3, 'nquo voluice', 18),
+(19, 2, 'niquo voluce', 19),
+(20, 4, 'niquice', 20);
 
 -- --------------------------------------------------------
 
@@ -215,10 +294,24 @@ CREATE TABLE `rooms` (
 INSERT INTO `rooms` (`id`, `room_type_id`, `rooms_number`) VALUES
 (1, 1, 1),
 (2, 2, 2),
-(3, 3, 3),
-(4, 4, 4),
-(5, 5, 5),
-(6, 14, 3);
+(3, 14, 2),
+(4, 22, 2),
+(5, 5, 1),
+(6, 6, 2),
+(7, 19, 1),
+(8, 8, 4),
+(9, 9, 3),
+(10, 11, 1),
+(11, 13, 2),
+(12, 17, 4),
+(13, 18, 3),
+(14, 14, 2),
+(15, 20, 1),
+(16, 21, 2),
+(17, 22, 1),
+(18, 24, 1),
+(19, 32, 2),
+(20, 35, 1);
 
 -- --------------------------------------------------------
 
@@ -238,37 +331,66 @@ CREATE TABLE `room_types` (
 --
 
 INSERT INTO `room_types` (`id`, `hotel_id`, `room_type`, `price`) VALUES
-(1, 4, 'dubbelrum', 1500),
-(2, 1, 'dubbelrum', 1500),
-(3, 2, 'dubbelrum', 1500),
-(4, 1, 'dubbelrum', 1500),
-(5, 3, 'dubbelrum', 1500),
-(6, 3, 'dubbelrum', 1500),
-(7, 3, 'dubbelrum', 1500),
-(8, 3, 'dubbelrum', 1500),
-(9, 3, 'dubbelrum', 1500),
-(10, 3, 'dubbelrum', 1500),
-(11, 2, 'enkelrum', 1000),
-(12, 1, 'enkelrum', 1000),
-(13, 2, 'enkelrum', 1000),
-(14, 4, 'enkelrum', 1000),
-(15, 2, 'enkelrum', 1000),
-(16, 2, 'enkelrum', 1000),
-(17, 1, 'enkelrum', 1000),
-(18, 2, 'enkelrum', 1000),
-(19, 1, 'enkelrum', 1000),
-(20, 4, 'enkelrum', 1000),
-(21, 1, 'familjerum', 2000),
-(22, 3, 'familjerum', 2000),
-(23, 2, 'familjerum', 2000),
-(24, 2, 'familjerum', 2000),
-(25, 4, 'familjerum', 2000),
-(26, 1, 'familjerum', 2000),
-(27, 2, 'familjerum', 2000),
-(28, 1, 'familjerum', 2000),
-(29, 2, 'familjerum', 2000),
-(30, 4, 'familjerum', 2000),
-(31, 4, 'dubbelrum', 2000);
+(1, 1, 'dubbelrum', 1500),
+(2, 2, 'dubbelrum', 1500),
+(3, 3, 'dubbelrum', 1500),
+(4, 4, 'dubbelrum', 1500),
+(5, 5, 'dubbelrum', 1500),
+(6, 6, 'dubbelrum', 1500),
+(7, 7, 'dubbelrum', 1500),
+(8, 8, 'dubbelrum', 1500),
+(9, 9, 'dubbelrum', 1500),
+(10, 10, 'dubbelrum', 1500),
+(11, 11, 'dubbelrum', 1500),
+(12, 12, 'dubbelrum', 1500),
+(13, 13, 'dubbelrum', 1500),
+(14, 14, 'dubbelrum', 1500),
+(15, 15, 'dubbelrum', 1500),
+(16, 16, 'dubbelrum', 1500),
+(17, 17, 'dubbelrum', 1500),
+(18, 18, 'dubbelrum', 1500),
+(19, 19, 'dubbelrum', 1500),
+(20, 20, 'dubbelrum', 1500),
+(21, 1, 'enkelrum', 1500),
+(22, 2, 'enkelrum', 1500),
+(23, 3, 'enkelrum', 1500),
+(24, 4, 'enkelrum', 1500),
+(25, 5, 'enkelrum', 1500),
+(26, 6, 'enkelrum', 1500),
+(27, 7, 'enkelrum', 1500),
+(28, 8, 'enkelrum', 1500),
+(29, 9, 'enkelrum', 1500),
+(30, 10, 'enkelrum', 1500),
+(31, 11, 'enkelrum', 1500),
+(32, 12, 'enkelrum', 1500),
+(33, 13, 'enkelrum', 1500),
+(34, 14, 'enkelrum', 1500),
+(35, 15, 'enkelrum', 1500),
+(36, 16, 'enkelrum', 1500),
+(37, 17, 'enkelrum', 1500),
+(38, 18, 'enkelrum', 1500),
+(39, 19, 'enkelrum', 1500),
+(40, 20, 'enkelrum', 1500),
+(41, 1, 'familjerum', 2000),
+(42, 2, 'familjerum', 2000),
+(43, 3, 'familjerum', 2000),
+(44, 4, 'familjerum', 2000),
+(45, 5, 'familjerum', 2000),
+(46, 6, 'familjerum', 2000),
+(47, 7, 'familjerum', 2000),
+(48, 8, 'familjerum', 2000),
+(49, 9, 'familjerum', 2000),
+(50, 10, 'familjerum', 2000),
+(51, 11, 'familjerum', 2000),
+(52, 12, 'familjerum', 2000),
+(53, 13, 'familjerum', 2000),
+(54, 14, 'familjerum', 2000),
+(55, 15, 'familjerum', 2000),
+(56, 16, 'familjerum', 2000),
+(57, 17, 'familjerum', 2000),
+(58, 18, 'familjerum', 2000),
+(59, 19, 'familjerum', 2000),
+(60, 20, 'familjerum', 2000);
 
 --
 -- Index för dumpade tabeller
@@ -341,43 +463,43 @@ ALTER TABLE `room_types`
 -- AUTO_INCREMENT för tabell `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT för tabell `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT för tabell `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT för tabell `hotels`
 --
 ALTER TABLE `hotels`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT för tabell `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT för tabell `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT för tabell `room_types`
 --
 ALTER TABLE `room_types`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- Restriktioner för dumpade tabeller
