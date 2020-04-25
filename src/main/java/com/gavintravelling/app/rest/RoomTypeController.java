1d8de317eb89de6d88ce99b9ff37bdab836c6e1c
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-04-25 15:49:27 +0200

    added method to get list of availablerooms for the given date for specified hotel

--- src/main/java/com/gavintravelling/app/rest/RoomTypeController.java
+++ src/main/java/com/gavintravelling/app/rest/RoomTypeController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -38,6 +40,13 @@ public class RoomTypeController {
     public ResponseEntity<RoomType> getRoomTypeById(@PathVariable long id) throws ResourceNotFoundException {
         RoomType roomType = getEntity(id);
         return ResponseEntity.ok().body(roomType);
+    }
+
+    @GetMapping("/{startDate}/{endDate}/{hotelId}")
+        public List<RoomType> getRoomsByBookedDate(@PathVariable String startDate, @PathVariable String endDate, @PathVariable Long hotelId) throws ParseException {
+        Date sd = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+        Date ed = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+        return roomTypeRepository.listAvailableRoomsBetweenDatesWithHotelId(sd, ed, hotelId);
     }
 
     @PostMapping
